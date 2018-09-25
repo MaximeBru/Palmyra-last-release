3e43616beca0e0ff8a304e1074364a012e17a405
Maxime Bru <mbru@vermeg.com>
2018-09-25 16:15:26 +0200

    index suite

--- pages/index.vue
+++ pages/index.vue
@@ -214,6 +214,172 @@
 
 
         <!-- section suivante  -->
+        <section class="Homefour expanded" id="target3" data-magellan-target="test3">
+      <div class="graphContainer" >
+
+        <div class="graphTitle" data-aos="fade-right" data-aos-offset="350" data-aos-duration="600">
+          <h3 class="red"><strong>A BASE FRAME</strong></h3>
+          <p><span class="red"><i class="fas fa-circle"></i></span> Agile </p>
+          <p><span class="pink"><i class="fas fa-circle"></i></span> Business Analysis</p>
+          <p><span class="grey"><i class="fas fa-circle"></i></span> Development</p>
+
+        </div>
+        <!-- ==========garph bar begin===== -->
+        <div class="igFrameBar">
+          <div class="iTab iTab1"></div>
+          <div class="iTab iTab2"></div>
+          <div class="iTab iTab3"></div>
+          <div class="iTab iTab4"></div>
+          <div class="iTab iTab5"></div>
+          <div class="iTab iTab6"></div>
+          <div class="igData " id="isdat1"> </div>
+          <div class="igData " id="isdat2"></div>
+          <div class="igData " id="isdat3"></div>
+          <div class="igData " id="isdat4"></div>
+          <div class="igData " id="isdat5"></div>
+          <div class="igData " id="isdat6"></div>
+
+        </div>
+        <!-- =======graph bar end=========== -->
+        <div class="artRigth">
+
+        </div>
+        <div class="sideImg" data-aos="fade-left" data-aos-offset="300" data-aos-duration="600">
+          <div class="BottomImg">
+            <img src="~/assets/images/palmImg2.png" alt="working on a computer">
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- last setction of homepage -->
+    <section class="QualityPart">
+      <article class="artRigth" data-aos="fade-right" data-aos-offset="450" data-aos-duration="600">
+        <img class="ToPres" src="~/assets/images/border.png" alt="ligne rouge entéte de section ">
+        <h2 class="TopTitle"><span class="red">PALMYRA </span><br>QUALITY PROCESS</h2>
+      </article>
+
+      <article class="Promote" data-aos="fade-right" data-aos-offset="250" data-aos-duration="600">
+          <p><span class="red">/ </span>Development</p>
+          <p> &ensp; Source Code Control at build time</p>
+          <p> &ensp; Code Review</p>
+          <p> &ensp; Static Security Audit</p>
+          <p> &ensp; Unit test is generalized for every development</p>
+        </article>
+        <article class="iconleft" data-aos="fade-left" data-aos-offset="250" data-aos-duration="600">
+          <img src="~/assets/images/icon1.png" alt="groupe d'icones">
+        </article>
+
+        <article class="tableArt" data-aos="fade-right" data-aos-offset="250" data-aos-duration="600">
+          <h3 class="red">A METHODOLOGY</h3>
+            <table cellspacing="20">
+                <tr>
+                  <th>Tutorials & training</th>
+                    <th>Procedures & Process</th>
+                    <th>Development</th>
+                    <th>Procedures & Process</th>
+                    <th>Development</th>
+                </tr>
+                <tr>
+                  <td>Business analysis</td>
+                    <td>152367</td>
+                    <td>147516</td>
+                    <td>148744</td>
+                    <td>147521</td>
+                </tr>
+                <tr>
+                  <td>Development</td>
+                    <td>148458</td>
+                    <td>147521</td>
+                    <td>256884</td>
+                    <td>784264</td>
+                </tr>
+                <tr>
+                  <td>Continuous Integration</td>
+                    <td>147459</td>
+                    <td>478556</td>
+                    <td>147521</td>
+                    <td>158963</td>
+                </tr>
+                <tr>
+                    <td>Procedure & Process</td>
+                      <td>478425</td>
+                      <td>458621</td>
+                      <td>147521</td>
+                      <td>147521</td>
+                  </tr>
+                </table>
+
+        </article>
+        <article class="Promote" id="SecdProm" data-aos="fade-right" data-aos-offset="350" data-aos-duration="600">
+            <p><span class="red">/ </span>Development</p>
+            <p> &ensp; Source Code Control at build time</p>
+            <p> &ensp; Code Review</p>
+            <p> &ensp; Static Security Audit</p>
+            <p> &ensp; Unit test is generalized for every development</p>
+          </article>
+
+          <article class="sideImg" id="QualImg" data-aos="fade-left" data-aos-offset="350" data-aos-duration="600">
+              <div class="BottomImg">
+                <img src="~/assets/images/compter&phone.png" alt="working on a computer">
+              </div>
+            </article>
+    </section>
+<section class="Benefit">
+    <article class="artRigth" data-aos="fade-right" data-aos-offset="400" data-aos-duration="600">
+        <img class="ToPres" src="~/assets/images/border.png" alt="ligne rouge entéte de section ">
+        <h2 class="TopTitle"><span class="red">BENEFITS </span><br>YOU ARE ?</h2>
+      </article>
+      <article class="iconleft" data-aos="fade-left" data-aos-offset="250" data-aos-duration="600">
+        <img src="~/assets/images/icon2.png" alt="second set of icon" id="BenefIcon">
+      </article>
+      <article class="benefitArt" data-aos="fade-right" data-aos-offset="200" data-aos-duration="600">
+        <h3 class="red">BUSINESS</h3>
+          <p><span class="red">/ </span>Faster Time to Market</p>
+          <p><span class="red">/ </span>State of the art user digital experience</p>
+          <p><span class="red">/ </span>Improved Customer services</p>
+          <p><span class="red">/ </span>Easy configuration</p>
+          <p><span class="red">/ </span>Continuous delivery circle</p>
+          <p><span class="red">/ </span>Avoid confusion and misinterpretations <br> caused by hundreds of specification pages</p>
+          <p><span class="red">/ </span>Avoid the tunnel effect</p>
+        <div class="borderTop"></div>
+        <div class="borderBottom"></div>
+      </article>
+
+      <article class="benefitArt" data-aos="fade-left" data-aos-offset="250" data-aos-duration="600">
+          <h3 class="red">DSI</h3>
+            <p><span class="red">/ </span>Faster Time to Market</p>
+            <p><span class="red">/ </span>State of the art user digital experience</p>
+            <p><span class="red">/ </span>Improved Customer services</p>
+            <p><span class="red">/ </span>Easy configuration</p>
+            <p><span class="red">/ </span>Continuous delivery circle</p>
+            <p><span class="red">/ </span>Avoid confusion and misinterpretations <br> caused by hundreds of specification pages</p>
+            <p><span class="red">/ </span>Avoid the tunnel effect</p>
+          <div class="borderTop"></div>
+          <div class="borderBottom"></div>
+        </article>
+
+        <article class="benefitArt" data-aos="fade-right" data-aos-offset="250" data-aos-duration="600">
+            <h3 class="red">DSI</h3>
+              <p><span class="red">/ </span>Faster Time to Market</p>
+              <p><span class="red">/ </span>State of the art user digital experience</p>
+              <p><span class="red">/ </span>Improved Customer services</p>
+              <p><span class="red">/ </span>Easy configuration</p>
+              <p><span class="red">/ </span>Continuous delivery circle</p>
+              <p><span class="red">/ </span>Avoid confusion and misinterpretations <br> caused by hundreds of specification pages</p>
+              <p><span class="red">/ </span>Avoid the tunnel effect</p>
+            <div class="borderTop"></div>
+            <div class="borderBottom"></div>
+          </article>
+
+          <article class="sideImg" id="BenefBotImg" data-aos="fade-left" data-aos-offset="300" data-aos-duration="600">
+            <div class="BottomImg">
+              <img src="~/assets/images/palmImg4.png" alt="working on a computer">
+              <img src="~/assets/images/littlePalmyra.png" alt="logo Palmyra" id="lastPalmyra">
+            </div>
+          </article>
+
+</section>
          <!-- end of page -->
     </div>
   </div>
