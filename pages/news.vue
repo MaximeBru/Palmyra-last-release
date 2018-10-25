d657b885232e0e91e60e83b821754053446373ba
Maxime Bru <mbru@vermeg.com>
2018-10-25 11:55:30 +0200

    fix

--- pages/news.vue
+++ pages/news.vue
@@ -3,14 +3,14 @@
 
   <section class="SecNews">
         <h2 class="NewsTilte">En Savoir Plus Sur Vermeg</h2>
-        <figure v-for="info in infos" :key="info.id" class="snip1208">
+        <figure v-for="info in infos.slice(0, 2)" :key="info.id" class="snip1208">
           <img :src="'http://localhost:1337' + info.image.url" alt="sample66" />
           <div class="date"><span class="day">{{$moment( info.date ).format('DD')}}</span><span class="month">{{$moment( info.date ).format('MMM')}}</span></div><i :class="info.logo.code"></i>
           <figcaption>
             <h3>{{ info.title }}</h3>
             <p>{{ info.description || 'No description provided.' }}</p>
             <button>Read More</button>
-          </figcaption></figcaption><a :href="'news/' + info.id"></a>
+          </figcaption></figcaption><a :href="'info/' + info.id"></a>
         </figure>
 
   </section>
