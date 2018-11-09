cd660807aafd9dc18ffff1a8652dcd9091e506e2
Maxime Bru <mbru@vermeg.com>
2018-11-09 13:57:25 +0100

    bug fix & integration de la modale verification du formulaire

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,39 +1,58 @@
 <template>
   <section class="container">
+
+  <!-- ... -->
+<h1 class="title">
+      Contact
+    </h1>
     <div class="content">
 
+        <b-btn v-b-modal.modal1>Demande d'Information</b-btn>
 
-    <form>
-
-        <label  name="name" >
+  <!-- Modal Component -->
+  <b-modal ref="myModalRef" v-b-modal.modallg variant="primary" hide-footer @ok="addToApi" id="modal1" title="Demande d'Information">
+    <p class="my-4">We'll send you the doc by mail</p>
+    <b-form>
+      <b-form-group id="exampleInputGroup1"
+                    label-for="name">
+        <b-form-label class="form-label" for="name">
           Name:
-        </label>
-        <input  name="name" id="name" v-model="Email.name" />
+        </b-form-label>
+        <b-form-input class="form-field" name="name" id="name" v-model="Email.name" />
+         </b-form-group>
 
+      <b-form-group
+                    label-for="email"
+                    description="We'll never share your email with anyone else.">
 
-
-        <label  name="email">
+        <b-form-label class="form-label" for="email">
           Email:
-        </label>
-        <input  name="email" id="email" v-model="Email.email"/>
-
-        <label name="firstname"  >
+        </b-form-label>
+        <b-form-input class="form-field" name="email" id="email" v-model="Email.email"/>
+      </b-form-group>
+      <b-form-group label-for="firstname">
+        <b-form-label class="form-label" for="message">
           firstname
-        </label>
-        <input  name="firstname" id="firstname" v-model="Email.firstname" />
-
-
-        <label  name="society" >
+        </b-form-label>
+        <b-form-input class="form-field" name="nickname" id="nickname" v-model="Email.firstname" />
+      </b-form-group>
+      <b-form-group label-for="firstname">
+        <b-form-label class="form-label" for="message">
           society:
-        </label>
-        <input  name="society" id="society" v-model="Email.society" />
-          <input type="submit"    @click="addToApi">
+        </b-form-label>
+        <b-form-input class="form-field" name="society" id="society" v-model="Email.society" />
+      </b-form-group>
+    </b-form>
+          <b-btn size="sm" class="float-right" block variant="outline-primary" @click="addToApi ">
+           Send
+         </b-btn>
 
-    </form>
+         <b-btn size="sm" class="float-right" block variant="outline-danger" @click="hideModal">
+           Close
+         </b-btn>
 
 
-
-
+  </b-modal>
 
 
     </div>
@@ -43,15 +62,16 @@
 
 <script>
 import Vue from 'vue'
-/* import BootstrapVue from 'bootstrap-vue' */
 import { mapState, mapActions, mapMutations } from 'vuex'
 /* import { init } from './shared' */
 import axios from 'axios'
 import VueMoment from 'vue-moment'
 import * as moment from 'moment'
+import Strapi from 'strapi-sdk-javascript/build/main'
+const apiUrl = 'http://localhost:1337/emails'
+const strapi = new Strapi(apiUrl)
 export default {
   head: {},
-
   data() {
     return {
       Email: {
@@ -80,12 +100,17 @@ export default {
         .catch(error => {
           console.log(error)
         })
+      this.$refs.myModalRef.hide()
+    },
+
+    hideModal() {
+      this.$refs.myModalRef.hide()
     }
   },
 
   computed: {
     ...mapState({
-      infos: state => state.email,
+      infos: state => state.infos,
       list: state => state.list
     })
   },
